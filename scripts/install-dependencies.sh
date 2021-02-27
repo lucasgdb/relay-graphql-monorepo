c1fc06e86863ee3432a5b1cf1a93a7261fada398
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2021-02-27 13:43:20 -0300

    fix path on install_dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,4 +2,4 @@
 
 sudo sh ./scripts/create_hosts.sh
 docker image build -t install_dependencies scripts
-docker container run --rm --user $(id -u):$(id -g) -v ${PWD}:/monorepo-react-node-postgres-ts -w /monorepo-react-node-postgres-ts install_dependencies:latest
+docker container run --rm --user $(id -u):$(id -g) -v ${PWD}:/graphql-relay-monorepo -w /graphql-relay-monorepo install_dependencies:latest
