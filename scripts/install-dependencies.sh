fb0c36ea7d34f2e04d15c179e71f3e821384cdd7
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2021-03-14 19:54:09 -0300

    change folder name

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,4 +2,4 @@
 
 sudo sh ./scripts/create_hosts.sh
 docker image build -t install_dependencies scripts
-docker container run --rm --user $(id -u):$(id -g) -v ${PWD}:/graphql-relay-monorepo -w /graphql-relay-monorepo install_dependencies:latest
+docker container run --rm --user $(id -u):$(id -g) -v ${PWD}:/relay-graphql-monorepo -w /relay-graphql-monorepo install_dependencies:latest
