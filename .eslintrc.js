408f69cfdac8532e16920022784ff71f580829e9
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2021-03-14 19:32:55 -0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const eslintConfig = require('@example/eslint-config');
+const eslintConfig = require('@example/eslint');
 
 module.exports = eslintConfig;
