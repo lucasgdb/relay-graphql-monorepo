da589f06d5ee8ffa47f9303e4e322fcca7500c67
Lucas Bittencourt <lucasgdbittencourt@gmail.com>
2021-03-14 19:44:48 -0300

    remove eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,67 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const eslintConfig = require('@example/eslint');
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:relay/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'relay', 'import'],
+  rules: {
+    'max-len': ['warn', { code: 120 }],
+    'no-restricted-globals': 'off',
+    'import/prefer-default-export': 'off',
 
-module.exports = eslintConfig;
+    'relay/generated-flow-types': 'off',
+    'relay/unused-fields': 'off',
+
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/button-has-type': 'off',
+
+    'import/no-unresolved': [2, { commonjs: true, amd: true }],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+};
