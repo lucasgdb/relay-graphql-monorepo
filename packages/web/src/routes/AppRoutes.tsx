71db3aaf4c938075aa8b98a8bdc75e4488d8ac18
lucasgdb <lucasgdbittencourt@gmail.com>
2022-02-17 12:39:31 -0300

    refactor: add react suspense

--- packages/web/src/routes/AppRoutes.tsx
+++ packages/web/src/routes/AppRoutes.tsx
@@ -1,18 +1,25 @@
 import { Route, Routes, HashRouter } from 'react-router-dom';
+import * as React from 'react';
 
-import EnvironmentLayout from '~/layout/EnvironmentLayout';
-import ErrorPage from '~/pages/Error/ErrorPage';
-import HomePage from '~/pages/Home/HomePage';
+import PageLoader from '~/components/PageLoader';
+
+const EnvironmentLayout = React.lazy(
+  () => import('~/layout/EnvironmentLayout')
+);
+const ErrorPage = React.lazy(() => import('~/pages/Error/ErrorPage'));
+const HomePage = React.lazy(() => import('~/pages/Home/HomePage'));
 
 export default function AppRoutes() {
   return (
     <HashRouter>
-      <Routes>
-        <Route element={<EnvironmentLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
+      <React.Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route element={<EnvironmentLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </React.Suspense>
     </HashRouter>
   );
 }
