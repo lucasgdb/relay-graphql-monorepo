a150ad06eef451510693e67aa4bf751fb3c95fb1
lucasgdb <lucasgdbittencourt@gmail.com>
2022-02-17 12:40:42 -0300

    chore: add css !important

--- packages/web/src/GlobalStyles.ts
+++ packages/web/src/GlobalStyles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   html, body {
-    background: #f5f5fb 0 0 no-repeat padding-box;
+    background: #f5f5fb 0 0 no-repeat padding-box !important;
     overflow-x: hidden;
   }
 `;
