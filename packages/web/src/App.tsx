c2dd64a0f71086427bae1a4dbd3b0fa85e79fc78
lucasgdb <lucasgdbittencourt@gmail.com>
2022-02-23 14:54:09 -0300

    refactor: remove styled global to mui theme

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,13 +3,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 
 import Providers from './Providers';
 import AppRoutes from './routes/AppRoutes';
-import GlobalStyles from './GlobalStyles';
 
 export default function App() {
   return (
     <Providers>
       <CssBaseline />
-      <GlobalStyles />
 
       <ErrorBoundary onActionClick={() => window.location.reload()}>
         <AppRoutes />
--- packages/web/src/Providers.tsx
+++ packages/web/src/Providers.tsx
@@ -13,6 +13,33 @@ import { environment } from './relay';
 dayjs.locale('pt-br');
 
 const theme = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          background: '#f5f5fb 0 0 no-repeat padding-box',
+          overflowX: 'hidden',
+
+          '&::-webkit-scrollbar-thumb': {
+            borderRadius: '8px',
+            backgroundColor: '#6b6b6b',
+            minHeight: '24px',
+            border: '3px solid #e8e6f2',
+          },
+          '&::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#9d9d9d',
+          },
+          '&::-webkit-scrollbar-corner': {
+            backgroundColor: '#e8e6f2',
+          },
+          '&::-webkit-scrollbar': {
+            backgroundColor: '#e8e6f2',
+          },
+        },
+      },
+    },
+  },
+
   palette: {
     primary: {
       main: '#200741',
