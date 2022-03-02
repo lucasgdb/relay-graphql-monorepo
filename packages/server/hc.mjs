a05dca3262f81059c6a4f187be7581f5ed8431bc
lucasgdb <lucasgdbittencourt@gmail.com>
2022-03-02 16:13:46 -0300

    chore: improve server healthcheck

--- packages/server/hc.mjs
+++
@@ -1,9 +0,0 @@
-import { get } from 'http';
-
-get('http://localhost:3000/api/hc', (res) => {
-  if (res.statusCode === 200) {
-    process.exit(0);
-  }
-
-  process.exit(1);
-}).on('error', () => process.exit(1));
