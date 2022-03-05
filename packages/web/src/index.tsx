d9eeeb11170e4b347510b4c47f17d9f62fdf0527
lucasgdb <lucasgdbittencourt@gmail.com>
2022-03-05 17:31:10 -0300

    refactor: change import from react-dom

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,14 +1,13 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 const container = document.getElementById('root');
-// @ts-expect-error it's an experimental feature of React, so it wasn't added to DefinitelyTyped yet.
-// eslint-disable-next-line import/namespace
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
+
 root.render(<App />);
 
 module.hot?.accept();
